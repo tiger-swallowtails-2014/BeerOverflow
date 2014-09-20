32865be5ad5bf82d0671f593d737ebb6cf0aea7e
NokoGary & Jessica Tung, Slayer of Gods, Destroyer of Worlds <pair+ghammell.jessicatung@devbootcamp.com>
2014-09-20 15:18:51 -0700

    add 'value' field to votes to dictate whether it is an upvote or downvote

--- BeerOverflow/db/seeds.rb
+++ BeerOverflow/db/seeds.rb
@@ -10,16 +10,16 @@ def seed
   Question.all.each do |question|
     3.times {question.answers.create(answer: Faker::Lorem.sentence, user_id: rand(5)+1)}
     3.times {question.comments.create(comment: Faker::Lorem.sentence, user_id: rand(5)+1)}
-    (rand(100)+1).times {question.votes.create(user_id: rand(5)+1)}
+    (rand(100)+1).times {question.votes.create(user_id: rand(5)+1, value: [1, -1].sample)}
   end
   
   Answer.all.each do |answer|
     3.times {answer.comments.create(comment: Faker::Lorem.sentence, user_id: rand(5)+1)}
-    (rand(100)+1).times {answer.votes.create(user_id: rand(5)+1)}
+    (rand(100)+1).times {answer.votes.create(user_id: rand(5)+1, value: [1, -1].sample)}
   end
   
   Comment.all.each do |comment|
-    (rand(100)+1).times {comment.votes.create(user_id: rand(5)+1)}
+    (rand(100)+1).times {comment.votes.create(user_id: rand(5)+1, value: [1, -1].sample)}
   end
 end
 
