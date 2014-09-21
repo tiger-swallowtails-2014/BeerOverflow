329ccca5ce0ffd5d4c81406824465d180ebd82ad
Jessica Tung <jessicatung@Jessicas-MacBook-Pro.local>
2014-09-21 16:48:25 -0700

    Add question_controller tests for #new -pg loads sucessfully -renders the new template

--- BeerOverflow/spec/controllers/questions_controller_spec.rb
+++ BeerOverflow/spec/controllers/questions_controller_spec.rb
@@ -39,9 +39,19 @@ describe QuestionsController do
 	end
 
 	context "#new" do
+		it "loads page successfully" do
+			get :new
+			expect(response).to be_success
+		end
+
+		it "renders the new template" do
+			get :new
+			expect(response).to render_template("new")
+		end
 	end
 
 	context "#create" do
+		
 	end
 
 	context "#edit" do
