668763e3db3488292343d94801039311de407bc4
Kyle Howell & <pair+kylehowell.travis75@devbootcamp.com>
2014-09-20 18:09:38 -0700

    pending model tests for answer

---
+++ BeerOverflow/spec/models/answer_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Answer do
+  it "is connected to a user"
+  it "cannot be created by the same user more than once"
+  it "is added to the database when created"
+  it "is changed in the database when updated"
+  it "is removed from the databse when deleted"
+end
