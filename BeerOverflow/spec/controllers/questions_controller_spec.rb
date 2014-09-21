d76f7adaa0e8228f84869a25eeb9703d3f7ce511
Jessica Tung <jessicatung@Jessicas-MacBook-Pro.local>
2014-09-21 15:51:18 -0700

    Add questions_controller specs for #index -loads page successfully -renders index template -assigns the right shit for @questions

---
+++ BeerOverflow/spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe QuestionsController do
+	before (:each) do
+		Question.delete_all
+	end
+
+	let!(:question) { FactoryGirl.create :question }
+
+	context "#index" do
+		it "loads page successfully" do
+			get :index
+			expect(response).to be_success
+			expect(response).to have_http_status(200)
+		end
+
+		it "renders the index template" do
+			get :index
+			expect(response).to render_template("index")
+		end
+
+		it "assigns @questions the correct shit" do
+			get :index
+			expect(assigns(:questions)).to include Question.last
+		end
+		
+	end
+
+	context "#show" do
+	end
+
+	context "#new" do
+	end
+
+	context "#create" do
+	end
+
+	context "#edit" do
+	end
+
+	context "#update" do
+	end
+
+	context "#destroy" do
+	end
+
+end
