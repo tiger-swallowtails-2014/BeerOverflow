4fee695345a5ce19d17729ba919c597431fbe618
NokoGary & Jessica Tung, Slayer of Gods, Destroyer of Worlds <pair+ghammell.jessicatung@devbootcamp.com>
2014-09-21 20:06:00 -0700

    ajaxified upvoting / downvoting, need to figure out how to get it to work without refreshing first

--- BeerOverflow/app/assets/javascripts/question.js
+++ BeerOverflow/app/assets/javascripts/question.js
@@ -1,11 +1,26 @@
 var Question = {
   bindEvents: function() {
     var self = this;
-    $('.show_container').on('ajax:success', '.new_comment', this.showCommentForm)
+    $('body').on('ajax:success', '.new_comment', this.showCommentForm)
+    $('body').on('ajax:success', '.button_to', this.upvote)
   },
   
   showCommentForm: function(e, data) {
     $(this).siblings('.comment_table').append(data)
+  },
+  
+  upvote: function() {  
+    start_count = $(this).parent().siblings().find('.sud_count')
+    button = $(this).children().find('input')
+    start_count_text = start_count.text().replace("suds", "")
+    
+    if ( button.attr("class") === "positive") {
+      new_count = parseInt(start_count_text) + 1
+      start_count.text( new_count + " suds" ) 
+    } else {
+      new_count = parseInt(start_count_text) - 1
+      start_count.text( new_count + " suds" ) 
+    }
   }
 }
 
