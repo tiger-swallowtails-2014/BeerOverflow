7c9e0ea8dcab15127e1e2e612cc77552417ed9e2
NokoGary & Jessica Tung, Slayer of Gods, Destroyer of Worlds <pair+ghammell.jessicatung@devbootcamp.com>
2014-09-21 21:34:10 -0700

    add turbo link jazz so refreshing is no longer necessary for upvote functionality

--- BeerOverflow/app/assets/javascripts/question.js
+++ BeerOverflow/app/assets/javascripts/question.js
@@ -1,32 +1,36 @@
-var Question = {
-  bindEvents: function() {
-    var self = this;
-    $('body').on('ajax:success', '.new_comment', this.showCommentForm)
-    $('body').on('ajax:success', '.button_to', this.vote)
-  },
-  
-  showCommentForm: function(e, data) {
-    $(this).siblings('.comment_table').append(data)
-  },
-  
-  vote: function(e, data) { 
-  if (data.indexOf("Success") === 0) {
-      start_count = $(this).parent().siblings().find('.sud_count')
-      button = $(this).children().find('input')
-      start_count_text = start_count.text().replace("suds", "")
+var ready;
+ready = function() {
+    var Question = {
+      bindEvents: function() {
+        var self = this;
+        $('body').on('ajax:success', '.new_comment', this.showCommentForm)
+        $('body').on('ajax:success', '.button_to', this.vote)
+      },
       
-      if ( button.attr("class") === "positive") {
-        new_count = parseInt(start_count_text) + 1
-        start_count.text( new_count + " suds" ) 
-      } else {
-        new_count = parseInt(start_count_text) - 1
-        start_count.text( new_count + " suds" ) 
+      showCommentForm: function(e, data) {
+        $(this).siblings('.comment_table').append(data)
+      },
+      
+      vote: function(e, data) { 
+      if (data.indexOf("Success") === 0) {
+          start_count = $(this).parent().siblings().find('.sud_count')
+          button = $(this).children().find('input')
+          start_count_text = start_count.text().replace("suds", "")
+          
+          if ( button.attr("class") === "positive") {
+            new_count = parseInt(start_count_text) + 1
+            start_count.text( new_count + " suds" ) 
+          } else {
+            new_count = parseInt(start_count_text) - 1
+            start_count.text( new_count + " suds" ) 
+          }
+        }
       }
     }
-  }
+
+  Question.bindEvents();
 }
 
-$(document).ready(function() {
-  Question.bindEvents();
-});
 
+$(document).ready(ready);
+$(document).on('page:load', ready);
