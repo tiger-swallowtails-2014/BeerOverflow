15d4d06c2fe4d085b41b1131abadec46c2c29968
NokoGary & Jessica Tung, Slayer of Gods, Destroyer of Worlds <pair+ghammell.jessicatung@devbootcamp.com>
2014-09-22 00:45:33 -0700

    added usernames to homepage questions

--- BeerOverflow/app/assets/javascripts/question.js
+++ BeerOverflow/app/assets/javascripts/question.js
@@ -13,20 +13,20 @@ ready = function() {
       },
       
       appendAnswer: function(e, data) {
-        console.log(data)
         $(this).siblings('.display_answers').append(data)
       },
       
       vote: function(e, data) { 
-      if (data.indexOf("Success") === 0) {
-          start_count = $(this).parent().siblings().find('.sud_count')
-          button = $(this).children().find('input')
-          start_count_text = start_count.text().replace("suds", "")
-          
+        if (data.indexOf("Success") === 0) {
+            start_count = $(this).parent().siblings().find('.sud_count')
+            button = $(this).children().find('input')
+            start_count_text = start_count.text().replace("suds", "")
+            
           if ( button.attr("class") === "positive") {
             new_count = parseInt(start_count_text) + 1
             start_count.text( new_count + " suds" ) 
-          } else {
+          } 
+          else {
             new_count = parseInt(start_count_text) - 1
             start_count.text( new_count + " suds" ) 
           }
